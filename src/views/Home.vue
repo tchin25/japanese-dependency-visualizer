f92a52aa6116c696de387ce6dc0e7a86fb5b7bdf
Thomas Chin <tchin25@gmail.com>
2020-11-15 21:23:23 -0500

    init vue3 with bulma

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="field">
+    <div class="control">
+      <input class="input" type="text" placeholder="Input" />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
