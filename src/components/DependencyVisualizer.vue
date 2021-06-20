31949889bd4cde64532ee300f76b7e25605fb456
Thomas Chin <tchin25@gmail.com>
2021-06-19 21:09:47 -0400

    extracted svg download to pure function

--- src/components/DependencyVisualizer.vue
+++ src/components/DependencyVisualizer.vue
@@ -108,6 +108,7 @@
 import { ref } from "vue";
 import svgPanZoom from "svg-pan-zoom";
 import initiateDraggable from "@/composition/initiateDraggable";
+import useDownloadSvg from "@/composition/useDownloadSvg";
 import { scaleOrdinal } from "d3-scale";
 import { schemeDark2 } from "d3-scale-chromatic";
 import { useState } from "@/api/sentenceFlow";
@@ -121,75 +122,13 @@ export default {
     const sentenceFlow = useState();
 
     const svg = ref(null);
-    const insert = ref(null);
 
-    const downloadSvg = () => {
-      const canvas = document.createElement("canvas");
-      const svgClone = svg.value.cloneNode(true);
-
-      // Get the bounds of the SVG content
-      var bbox = svg.value.firstChild.getBBox();
-      const width = bbox.x + bbox.width + bbox.x;
-      // The +4 is to account for padding of last node
-      const height = bbox.y + bbox.height + bbox.y + 4;
-
-      // Update the width and height using the size of the contents
-      svgClone.setAttribute("width", width);
-      svgClone.setAttribute("height", height);
-
-      // Place generated svg into view
-      svgClone.firstChild.setAttribute("style", "");
-      svgClone.firstChild.setAttribute("transform", "");
-
-      // Prepare canvas
-      const ctx = canvas.getContext("2d");
-      const paddingX = 8;
-      const paddingY = 8;
-      canvas.width = width + 2 * paddingX;
-      canvas.height = height + 2 * paddingY;
-      ctx.fillStyle = "white";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-      // Convert SVG to data url
-      const DOMURL = window.URL || window.webkitURL || window;
-      const img = new Image();
-      const s = new XMLSerializer();
-      const imageData = s.serializeToString(svgClone);
-      const svgBlob = new Blob([imageData], {
-        type: "image/svg+xml;charset=utf-8",
-      });
-      const url = DOMURL.createObjectURL(svgBlob);
-
-      img.onload = () => {
-        ctx.drawImage(img, paddingX, paddingY);
-        DOMURL.revokeObjectURL(url);
-
-        const imgURI = canvas
-          .toDataURL("image/png")
-          .replace("image/png", "image/octet-stream");
-
-        const evt = new MouseEvent("click", {
-          view: window,
-          bubbles: false,
-          cancelable: true,
-        });
-
-        const a = document.createElement("a");
-        a.setAttribute("download", "dependency_diagram.png");
-        a.setAttribute("href", imgURI);
-        a.setAttribute("target", "_blank");
-
-        a.dispatchEvent(evt);
-      };
-
-      img.src = url;
-    };
+    const downloadSvg = useDownloadSvg(svg);
 
     return {
       ...sentenceFlow,
       ...initiateDraggable(svg),
       svg,
-      insert,
       downloadSvg,
     };
   },
